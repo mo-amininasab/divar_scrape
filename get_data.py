f44898538e87c46658e676fadea6d8ed9f143553
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-08-16 12:49:42 +0430

    update

--- get_data.py
+++ get_data.py
@@ -1,3 +1,4 @@
+import requests
 import time
 import csv
 
@@ -11,8 +12,15 @@ COLUMNS = [
     'price'
 ]
 
-with open('divar_apartments.txt', 'r', encoding='utf-8') as f:
+# with open('links_test.txt', 'r', encoding='utf-8') as f:
+with open('./links/divar_apartment_links.txt', 'r', encoding='utf-8') as f:
   links = f.readlines()
+  # for i in range(len(links)):
+  #   print(links[i].strip())
+  # i = 0
+  # while True:
+    # print(links[i].strip())
+    # i += 1
 print(f'number of links: {len(links)}')
 
 i = 0
@@ -21,24 +29,47 @@ i = 0
 def main():
   global i
 
-  with open('divar_apartments.csv', 'w', encoding='utf-8') as csv_file:
+  with open('divar_apartments_test.csv', 'w', encoding='utf-8') as csv_file:
     writer = csv.DictWriter(csv_file,
                             fieldnames=COLUMNS,
                             delimiter=',',
                             lineterminator='\n')
     writer.writeheader()
 
-    for link in links:
-      print(f'i = {i}')
-      link = link.strip()
-      data = get_data(f'{URL}{link}')
+    while True:
+      try:
+        link = links[i].strip()
+        print(f'link {i+1}: {link}')
+      except IndexError:
+        print('End of links')
+        break
+      try:
+        data = get_data(link)
+      except requests.HTTPError as e:
+        print('Error: 404 page')
+        i += 1
+        continue
+      except requests.exceptions.ConnectionError as e:
+        print('Connection Error:', e)
+        time.sleep(5)
+        main()
 
       writer.writerow(data)
-
-      time.sleep(1)
-      print(f'{link} scraped.')
-
       i += 1
+      time.sleep(1)
+      print(f'link {i} scraped.')
+
+    # for link in links:
+    #   print(f'i = {i}')
+    #   link = link.strip()
+    #   data = get_data(f'{URL}{link}')
+
+    #   writer.writerow(data)
+
+    #   time.sleep(1)
+    #   print(f'{link} scraped.')
+
+    #   i += 1
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -24,6 +24,7 @@ def p2e(text):
 
 def get_data(url):
   request = requests.get(url)
+  request.raise_for_status()
   soup = BeautifulSoup(request.text, 'html.parser')
   # with open('test.html', 'w', encoding='utf-8') as f:
   #   f.write(soup.prettify())
@@ -31,48 +32,94 @@ def get_data(url):
   # with open('test.html', 'r', encoding='utf-8') as f:
   #   soup = BeautifulSoup(f.read(), 'html.parser')
   # with open('test2.html', 'w', encoding='utf-8') as f:
-  title = soup.find('div', attrs={'class': 'kt-page-title__title kt-page-title__title--responsive-sized'}).text
-  shit = soup.find_all('span', attrs={'class': 'kt-group-row-item__value'})
-  area = (shit[0].contents[0])
-  area = p2e(area)
-  construct_date = (shit[1].contents[0])
-  construct_date = p2e(construct_date)
-  rooms = (shit[2].contents[0])
-  rooms = p2e(rooms)
-  has_elevator = (shit[3].contents[0])
-  has_parking = (shit[4].contents[0])
-  has_warehouse = (shit[5].contents[0])
+  title = soup.find(
+      'div',
+      attrs={
+          'class':
+          'kt-page-title__title kt-page-title__title--responsive-sized'
+      }).text
 
-  shit2 = soup.find_all('p', attrs={'class': 'kt-unexpandable-row__value'})
-  price = shit2[0].contents[0].split('تومان')[0].strip()
-  price = p2e(price)
-  price_per_meter = shit2[1].contents[0].split('تومان')[0].strip()
-  price_per_meter = p2e(price_per_meter)
-  floor = shit2[3].contents[0].split(' ')[0]
-  floor = p2e(floor)
+  # finding area, construct_date, rooms
+  list1 = soup.find_all('div', attrs={'class': 'kt-group-row-item kt-group-row-item--info-row'})
+  area = p2e(list1[0].contents[1].text.strip())
+  construct_date = p2e(list1[1].contents[1].text.strip())
+  rooms = list1[2].contents[1].text.strip()
+  rooms = 0 if 'بدون اتاق' in rooms else  p2e(rooms)
+
+  # finding has_elevator, has_parking, has_warehouse
+  list2 = soup.find_all('span', attrs={'class': 'kt-group-row-item__value kt-body kt-body--stable'})
+  has_elevator = list2[0].text.strip()
+  has_parking = list2[1].text.strip()
+  has_warehouse = list2[2].text.strip()
+
+
+  # finding area, construct_date, rooms, has_elevator, has_parking, has_warehouse
+  # list1 = soup.find_all('span', attrs={'class': 'kt-group-row-item__value'})
+  # area = (list1[0].text)
+  # area = p2e(area)
+  # construct_date = (list1[1].text)
+  # construct_date = p2e(construct_date)
+  # rooms = (list1[2].text)
+  # rooms = p2e(rooms)
+  # has_elevator = (list1[3].text)
+  # has_parking = (list1[4].text)
+  # has_warehouse = (list1[5].text)
+
+  # finding price, price_per_meter, floor
+  list3 = soup.find_all('div', attrs={'class': 'kt-base-row kt-base-row--large kt-unexpandable-row'})
+  price = None
+  price_per_meter = None
+  floor = None
+  for item in list3:
+    if 'قیمت کل' in item.contents[0].text:
+      price = p2e(item.contents[1].text.split(' ')[0].strip())
+    if 'قیمت هر متر' in item.contents[0].text:
+      price_per_meter = p2e(item.contents[1].text.split(' ')[0].strip())
+    if 'طبقه' in item.contents[0].text:
+      if 'همکف' in item.contents[1].text:
+        floor = 0
+      else:
+        floor = p2e(item.contents[1].text.split(' ')[0].strip())
+
+  
+  # with open('test2.html', 'w', encoding='utf-8') as f:
+  #   print(price, price_per_meter, floor)
+
+
+
+  # list3 = soup.find_all('p', attrs={'class': 'kt-unexpandable-row__value'})
+  # price = list3[0].contents[0].split('تومان')[0].strip()
+  # price = p2e(price)
+  # price_per_meter = list3[1].contents[0].split('تومان')[0].strip()
+  # price_per_meter = p2e(price_per_meter)
+  # floor = list3[3].contents[0].split(' ')[0].strip()
+  # floor = p2e(floor)
+
 
   type = soup.find_all('a', attrs={'class': 'kt-breadcrumbs__link'})[2].text
-  city = soup.find('div', attrs={'class': 'kt-page-title__subtitle kt-page-title__subtitle--responsive-sized'}).text,
-  city = city[0].split('|')[0].split('،')[1]
+  city = soup.find(
+      'div',
+      attrs={
+          'class':
+          'kt-page-title__subtitle kt-page-title__subtitle--responsive-sized'
+      }).text,
+  city = city[0].split('|')[0].split('،')[1].strip()
 
   data = {
-    'title': title,
-    'area': area,
-    'construct_date': construct_date,
-    'rooms': rooms,
+      'url': url,
 
-    'price': price,
-    'price_per_meter': price_per_meter,
-    'floor': floor,
-
-    'has_elevator': False if 'ندارد' in has_elevator else True,
-    'has_parking': False if 'ندارد' in  has_parking else True,
-    'has_warehouse': False if 'ندارد' in  has_warehouse else True,
-
-    'type': type,
-    'city': city,
-
-    'url': url
+      'title': title,
+      'area': area,
+      'construct_date': construct_date,
+      'rooms': rooms,
+      'price': price,
+      'price_per_meter': price_per_meter,
+      'floor': floor,
+      'has_elevator': False if 'ندارد' in has_elevator else True,
+      'has_parking': False if 'ندارد' in has_parking else True,
+      'has_warehouse': False if 'ندارد' in has_warehouse else True,
+      'type': type,
+      'city': city,
   }
 
   return data
